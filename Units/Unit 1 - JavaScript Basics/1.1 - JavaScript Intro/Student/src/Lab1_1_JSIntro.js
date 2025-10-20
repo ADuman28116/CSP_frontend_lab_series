e739d0095046577cab9d545ec508ccaf18668c30
ADuman28116 <aduman28116@columbushs.com>
2025-10-20 09:28:56 -0400

    completed to do one

--- Units/Unit 1 - JavaScript Basics/1.1 - JavaScript Intro/Student/src/Lab1_1_JSIntro.js
+++ Units/Unit 1 - JavaScript Basics/1.1 - JavaScript Intro/Student/src/Lab1_1_JSIntro.js
@@ -4,6 +4,7 @@
    =========================================================== */
 
 // TODO 1: Print your full name with console.log()
+console.log("Alec");
 
 
 // TODO 2: Declare variable favoriteFood, assign "Steak", then reassign "Pizza"
