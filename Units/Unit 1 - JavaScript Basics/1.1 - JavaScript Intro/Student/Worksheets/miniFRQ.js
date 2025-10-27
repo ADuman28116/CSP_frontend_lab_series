7132493659944646324162e954329c5b468d0881
ADuman28116 <aduman28116@columbushs.com>
2025-10-27 13:37:50 -0400

    finished all todos

---
+++ Units/Unit 1 - JavaScript Basics/1.1 - JavaScript Intro/Student/Worksheets/miniFRQ.js
@@ -0,0 +1,8 @@
+let height = 100;
+let width = 40;
+
+let area = height * width;
+let perimeter = 2 * (height + width);
+
+console.log(`Area of retangle $(area)`)
+console.log(`Perimeter of rectangle`)
--- Units/Unit 1 - JavaScript Basics/1.2 - Data Types in JavaScript/Student/1.2.1/src/Lab1_2_DataTypes.js
+++ Units/Unit 1 - JavaScript Basics/1.2 - Data Types in JavaScript/Student/1.2.1/src/Lab1_2_DataTypes.js
@@ -3,16 +3,43 @@
 */
 
 // TODO 1: Create variables age (34) and price (12.99), print both
+let age = 34;
+console.log("Value: " + price + " TypeOf: " + typeof price);
+
+let price = 12.99;
+console.log("Value: " + price + " TypeOf: " + typeof price);
+
+
 // TODO 1b: Divide "Sandra" by 2 and print result (expect NaN)
+let result = "Sandra" / 2;
+console.log("Result: " / 2 " TypeOf: " / typeof result);
 
 // TODO 2: Print basic math ops: + - * /
+console.log("Basic math ops: " + - * / " TypeOf: " + - * / typeof basic math ops);
+
+
 // TODO 2b: Use exponentiation (2 ** 3) and modulo (7 % 2)
+console.log(2**3);
+
+console.log(7 % 2);
 
 // TODO 3: Assignment operators with score variable
+score = 100
+console.log("Assign the value 100 to the variable score")
 
 // TODO 4: Expressions with operator precedence
+let result = ((7 + 5) / 3) - 8; 
+console.log(result);
+
+((7 + 5) / 3) - 8
+12/3 - 8
+4 - 8
+-4
 
 // TODO 5: Strings (single, double, backticks)
+let single = "Hello World!";
+let double = "Hello World!";
+let backticks = "Hello World!";
 
 // TODO 6: String properties and methods
 // - greeting.length
@@ -20,6 +47,32 @@
 // - indexOf("sad")
 // - repeat()
 // - substring, substr, slice examples
+
+let greeting = "Hello World!";
+console.log(greeting.length); // Output: 12
+console.log(greeting.charAt(0)); // Output: H
+console.log(greeting.charAt(5)); // Output: ' ' (space)
+console.log(greeting[3]); // Output: l
+
+let text = "This is a sad story.";
+console.log(text.indexOf("sad")); // Output: 10
+
+let word = "ha";
+console.log(word.repeat(3)); // Output: hahaha
+
+let sentence = "JavaScript is fun!";
+console.log(sentence.substring(0, 10)); // Output: JavaScript
+console.log(sentence.substring(11)); // Output: is fun!
+console.log(sentence.substring(5, 0)); // Output: JavaS (arguments are swapped)
+
+sentence = "JavaScript is fun!";
+console.log(sentence.substr(0, 10)); // Output: JavaScript
+console.log(sentence.substr(11, 2)); // Output: is
+
+sentence = "JavaScript is fun!";
+console.log(sentence.slice(0, 10)); // Output: JavaScript
+console.log(sentence.slice(-4)); // Output: fun!
+console.log(sentence.slice(0, -5)); // Output: JavaScript is
 
 // 🚀 Stretch Goals
 // - Even number check using %
